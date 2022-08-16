2bad091b826437181fcb2ad695b80b2622dbe403
Mateusz Wala <mateusz.wala2734@gmail.com>
2022-08-17 01:11:44 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const port = process.env.PORT || 3000
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({message: "Welcome to the Bulk Mailer"});
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}.`);
+});
